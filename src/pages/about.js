dee42c0f48d8a3dbe0731aa0616a4969a4bf2b87
Derek Kim <ddkim1024@gmail.com>
2021-12-12 22:06:58 -0600

    Updated my story in About Me

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,10 +17,13 @@ import Layout from "../components/layout/layout";
 import ProfileImg from "../images/profile.jpg";
 
 // Content Data
-const paragraph0 =
-  "What brings someone trained in Microbiology to the world of software development? To be frank, I thought I had everything figured out when I enrolled in grad school. One day, I started reading my brother’s intro Java textbook out of curiosity and I was introduced to a new world. I fell in love with the problem-solving nature and sheer versatility of programming. As a life-long nerd, the desire to build technologies that play an increasingly larger part of our lives kept growing inside me.";
-const paragraph1 =
-  "Thanks to the breathtaking amount of resources available online and helpful communities, I was able to learn from zero and dip my feet in open-source development, which cemented my passion for this craft. I still have a lot to learn as a self-taught developer but every new piece of technology I learn is an adventure I am excited to take part in. I’d love to be able to put my passion towards tackling real-world challenges.";
+const CONTENT = [
+  "In 2017, my eyes opened to new possibilities. Until I started my Master's, I thought everything was figured out; I'd go to university, finish grad school, and do microbiology just like my parents. I always loved to learn new things but grad school helped me find an unexpected passion; coding.",
+  "I cracked open my terminal on Ubuntu that I installed on a whim and made my foray into development with a script that automated entering book giveaways on Goodreads. Over time, I found myself spending more time learning development than what I was supposed to be studying and finally made the decision that this was my true calling.",
+  "Transitioning to become a software developer on my own was challenging but also incredibly eye opening. In the 2 years I spent isolated in my bedroom, I was immersed in the vibrant online dev and open-source communities. I never once felt like I was making this journey alone.",
+  "Finally landing a professional dev role and being able to work in a team with other developers to create products meant for real people is a dream come true. If I wasn't sure before, I now know for certain that this is what I want to be doing.",
+  "I wake up each morning with the goal of writing better code than I did the previous day. I'm late to the game but I am fully dedicated to honing my codecraft and I strive to be a teammate that can be depended on.",
+];
 
 const AboutPage = ({ data }) => {
   const [isLargerThan1024] = useMediaQuery("(min-width: 1024px)");
@@ -50,12 +53,11 @@ const AboutPage = ({ data }) => {
         </Flex>
         {/* Right Section */}
         <Box>
-          <Text fontSize="xl" padding="2">
-            {paragraph0}
-          </Text>
-          <Text fontSize="xl" padding="2">
-            {paragraph1}
-          </Text>
+          {CONTENT.map((paragraph, index) => (
+            <Text key={index} fontSize="xl" padding="2">
+              {paragraph}
+            </Text>
+          ))}
         </Box>
       </Box>
       <Flex flexDirection="column" my="8">
